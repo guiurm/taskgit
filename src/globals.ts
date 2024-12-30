0fab9d57bcadec3f38885d4b84d902d8a11c63a0
guiurm <guiurm@gmail.com>
2024-12-30 12:17:33 +0100

    chore(globals): adds package.json data for name and version of the project

--- src/globals.ts
+++ src/globals.ts
@@ -1,11 +1,15 @@
 import { tmpdir } from 'node:os';
-import path from 'node:path';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { rf } from './services/fileService';
 
-const TMP_DIR = path.join(tmpdir(), 'taskgit');
-const TMP_PATCH_DIR = path.join(TMP_DIR, 'patch');
+const TMP_DIR = join(tmpdir(), 'taskgit');
+const TMP_PATCH_DIR = join(TMP_DIR, 'patch');
 
-const NAME = '@guiurm/taskgit';
-const VERSION = '1.0.0-rc.2';
+const pkg = JSON.parse(rf(join(fileURLToPath(import.meta.url), '..', '..', './package.json')));
+
+const NAME = pkg.name;
+const VERSION = pkg.version;
 const VERSION_NAME = `${NAME}@${VERSION}`;
 
 const IS_DEV = process.env.NODE_ENV !== 'production';
@@ -31,7 +35,7 @@ const COMMIT_STANDARD_TYPES = [
         description: 'Cambios en el formato o estilo del código, sin afectar la funcionalidad.'
     },
     {
-        icon: '♻️',
+        icon: '♻️ ',
         name: 'refactor',
         description: 'Reestructuración del código para mejorar su calidad o rendimiento, sin cambiar su comportamiento.'
     },
@@ -52,13 +56,13 @@ const COMMIT_STANDARD_TYPES = [
             'Tareas de mantenimiento, configuración o administración, que no afectan la funcionalidad del proyecto.'
     },
     {
-        icon: '🏗️',
+        icon: '🏗️ ',
         name: 'build',
         description:
             'Cambios relacionados con la construcción del proyecto, como dependencias, compilación o configuración.'
     },
     {
-        icon: '⚙️',
+        icon: '⚙️ ',
         name: 'ci',
         description: 'Cambios en la configuración de integración continua o en los pipelines de CI/CD.'
     },
@@ -84,7 +88,7 @@ const COMMIT_STANDARD_TYPES = [
     },
     { icon: '⏪', name: 'revert', value: 'revert', description: 'Deshacer cambios realizados en un commit anterior.' },
     {
-        icon: '⚒️',
+        icon: '⚒️ ',
         name: 'wip',
         description: 'Trabajo en progreso, commit incompleto que refleja cambios aún en desarrollo.'
     }
